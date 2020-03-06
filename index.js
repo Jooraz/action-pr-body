774bfb87dcde46de76426d87fc47e66bec06a78c
Jooraz <jurkiewicztj@gmail.com>
2020-03-06 15:48:28 +0100

    Code for checking regex on body of PR

---
+++ index.js
@@ -0,0 +1,34 @@
+const validEvent = ["pull_request"];
+
+function validateTitlePrefix(body, regexString) {
+    const regex = new RegExp(regexString);
+    return regex.test(body);
+}
+
+async function exec() {
+    try {
+        const eventName = github.context.eventName;
+        core.info(`Event name: ${eventName}`);
+        if (validEvent.indexOf(eventName) < 0) {
+            core.setFailed(`Invalid event: ${eventName}`);
+            return;
+        }
+
+        const title = github.context.payload.pull_request.body;
+        core.info(`Pull Request title: "${body}"`);
+
+        const regexElements = core.getInput("regex_checks");
+        core.info(`Regex check: ${regexElements}`);
+        if (regexElements.length > 0 &&
+            !regexElements.split(",").every(el => testRegex(body, el))) {
+            core.setFailed(
+                `Pull Request body did not match one of the regex rules - ${regexElements}`
+            );
+            return;
+        }
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+exec();
